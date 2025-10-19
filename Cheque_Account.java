b427cc0953d802b0b37ff1d7e4e6c7f8c08c9373
Heivin <24177961a@common.cpce-polyu.edu.hk>
2025-10-19 22:20:43 +0800

    Cheque basic method

--- Cheque_Account.java
+++ Cheque_Account.java
@@ -1,12 +1,17 @@
 public class Cheque_Account extends Account
 {
-    private double availableBalance;
-    private double totalBalance;
-    private double interest_rate;
+    private double AvailableBalance;
+    private double TotalBalance;
+    private double Interest_rate;
     public Cheque_Account(int AccountNumber, int PIN, double AvailableBalance, double TotalBalance)
     {
 
         super(AccountNumber, PIN, AvailableBalance, TotalBalance);
     }
-        
+    public double Interest_rate(){
+        return Interest_rate;
+    }
+    public void setLimit_per_cheque(double rate){
+        Interest_rate = rate;
+    }
 }
