24998d6d8a83fefaddfcf479a290cfa82bf0ef61
Heivin <24177961a@common.cpce-polyu.edu.hk>
2025-10-19 22:18:04 +0800

    Basic method for Saving Account

--- Saving_Account.java
+++ Saving_Account.java
@@ -1,11 +1,17 @@
 public class Saving_Account extends Account
 {
-    private double availableBalance;
-    private double totalBalance;
-    private double imit_per_cheque;
+    private double AvailableBalance;
+    private double TotalBalance;
+    private double Limit_per_cheque = 0.0025;
     public Saving_Account(int AccountNumber, int PIN, double AvailableBalance, double TotalBalance)
     {
 
         super(AccountNumber, PIN, AvailableBalance, TotalBalance);
+    }
+    public double getLimit_per_cheque(){
+        return Limit_per_cheque;
+    }
+    public void setLimit_per_cheque(double Limit){
+    Limit_per_cheque = Limit;
     }
 }
