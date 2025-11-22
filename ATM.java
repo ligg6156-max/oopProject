e034c047a2448af3e8d171cb964e1e67b4d2cf1a
ccnrayy <minionsraymond@gmail.com>
2025-11-22 23:27:35 +0800

    Fix consistency of entering account and pin when login

--- ATM.java
+++ ATM.java
@@ -343,7 +343,7 @@ public class ATM {
             return;
         }
         screen.clear();
-        screen.displayMessage("\nEnter your PIN: "); // prompt for PIN
+        screen.displayMessage("\n>Enter your PIN: "); // prompt for PIN
         keypad.setPasswordMode(true); // Enable password masking
         int pin = keypad.getIntInput(); // input PIN
         keypad.setPasswordMode(false); // Disable password masking
