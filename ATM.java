0dc84398e524e9537694ff10570f40adbacd8860
heivin987 <24177961a@common.cpce-polyu.edu.hk>
2025-11-23 12:45:46 +0800

    fix: display error message for invalid account number input

--- ATM.java
+++ ATM.java
@@ -341,6 +341,7 @@ public class ATM {
         int accountNumber = keypad.getIntInput(); // input account number
         if (accountNumber == CANCELED) {
             screen.clear();
+            screen.displayMessageLine("Invalid account number or PIN. Please try again.");
             return;
         }
         screen.clear();
@@ -351,7 +352,6 @@ public class ATM {
         if (accountNumber == CANCELED) {
             screen.clear();
             screen.displayMessageLine("Invalid account number or PIN. Please try again.");
-            screen.MessagePopup("Invalid account number or PIN. Please try again.");
             return;
         }
 
